3980d9e4cef77ff3d6e6bf6d2194caa42a8482b5
Debasis08 <patrodebasis8@gmail.com>
2024-02-10 20:39:54 +0530

    Finalized the todoform file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {TodoProvider} from './Contexts'
+import TodoForm from './Components/TodoForm'
+import TodoItem from './Components/TodoItem'
 import './App.css'
 
 function App() {
 
     const [todos, setTodos] = useState([])
 
+    //Add a todo
     const addTodo = (todo) => {
       setTodos((prev) => [{id: Date.now(), ...todo}, ...prev] )
     }
+
+    //Update a todo
+    const updatedTodo = (id, todo) => {
+      setTodos( (prev) => prev.map((prevTodo) =>  (prevTodo.id === id ? todo : prevTodo)))
+    }
+
+    //delete a todo
+    const deleteTodo = (id) => {
+      setTodos((prev) => prev.filter((todo) => todo.id !== id))
+    }
+
+    //Toggle Checkbox
+    const toggleComplete = (id) => {
+      setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
+    }
+
+
+    useEffect(() => {
+      const todos = JSON.parse(localStorage.getItem("todos"))
+
+      if (todos && todos.length > 0) {
+        setTodos(todos)
+      }
+    }, [])
+
+    useEffect(() => {
+      localStorage.setItem("todos", JSON.stringify(todos))    
+    }, [todos])
+    
+
 
   return (
 
@@ -18,9 +51,17 @@ function App() {
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
+                        <TodoForm />
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo) => (
+                          <div key={todo.id}
+                          className='w-full'>
+                            <TodoItem todo = {todo} />
+                            
+                          </div>
+                        ))}
                     </div>
                 </div>
             </div>
